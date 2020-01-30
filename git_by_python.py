9df2c7aa7dac4fe5fb235c03f79221c1ba2d87ef
dxp432 <dxp432@163.com>
2020-01-30 16:44:43 +0800

    python commit

---
+++ git_by_python.py
@@ -0,0 +1,20 @@
+import os
+import time
+
+def my_cmd(my_cmd):
+    res = os.popen(my_cmd)
+    return res.read()
+
+if __name__ == '__main__':
+    my_r = my_cmd(r'D:')
+    print(my_r)
+    my_r = my_cmd(r'cd D:\myPython\20200130_git_by_python')
+    print(my_r)
+    my_r = my_cmd(r'git status')
+    print(my_r)
+    my_r = my_cmd(r'git add .')
+    print(my_r)
+    my_r = my_cmd(r'git commit -m "python commit"')
+    print(my_r)
+    my_r = my_cmd(r'git push -u origin master')
+    print(my_r)
