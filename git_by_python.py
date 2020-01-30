fb82ca33fb1bc2b10350db02dfe29a78eadb3851
dxp432 <dxp432@163.com>
2020-01-30 16:59:01 +0800

    python commit

--- git_by_python.py
+++ git_by_python.py
@@ -1,9 +1,11 @@
 import os
 import time
 
+
 def my_cmd(my_cmd):
     res = os.popen(my_cmd)
     return res.read()
+
 
 if __name__ == '__main__':
     my_r = my_cmd(r'D:')
@@ -15,16 +17,14 @@ if __name__ == '__main__':
     print('now git add .')
     my_r = my_cmd(r'git add .')
     print(my_r)
-    time.sleep(5)
+
     print('now git commit')
     my_r = my_cmd(r'git commit -m "python commit"')
     print(my_r)
-    time.sleep(9)
 
     print('now remote')
     my_cmd(r'git remote add origin git@github.com:dxp432/git_by_python.git')
     print(my_r)
-    time.sleep(5)
 
     print('now git push')
     my_r = my_cmd(r'git push -u origin master')
